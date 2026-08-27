7eccfd4e93661e4fa077d9ecfcc47e9d20e7771c
Areeb Khan <techdevmalik@gmail.com>
2026-08-26 23:20:16 -0700

    model: add unique phone number field to user entity

--- backend/src/main/java/com/areeb/backend/model/User.java
+++ backend/src/main/java/com/areeb/backend/model/User.java
@@ -13,12 +13,13 @@ public class User {
     @Column(unique = true, nullable = false)
     private String username;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true)
     private String email;
 
     @Column(nullable = false)
     private String password;
 
+    @Column(unique = true)
     private String phoneNumber;
 
     public User() {}
